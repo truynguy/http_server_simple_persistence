7544a91d9c184e57d285cc8f2d7cb5b05121bbb9
Truyen Nguyen <truyen.m.nguyen@gmail.com>
2015-05-12 20:27:59 -0700

    Add more tests to the server

--- test/test_http.js
+++ test/test_http.js
@@ -9,23 +9,59 @@ var expect = chai.expect;
 chai.use(chaihttp);
 
 describe('notes REST api', function(){
-    it('should be able to create a new note', function(done) {
+  it('GET request', function(done) {
       chai.request('localhost:3000')
-        .post('/api/data')
-        .send({noteBody: 'hello there '})
-        .end(function(err, res) {
-          expect(err).to.eql(null);
-          expect(res.body.msg).to.eql('success');
-          done();
-    });
-
-    it('should get an array of notes', function(done) {
-      chai.request('localhost:3000')
-      .get('/api/data')
+      .get('/api/quotes')
       .end(function(err, res) {
         expect(err).to.eql(null);
-        done(); 
+        expect(typeof res.body).to.eql('object');
+        expect(Array.isArray(res.body)).to.be.eql(true);
+        done();
       });
     });
-  });
+
+    it('POST request', function(done) {
+      var testQuote = new Quote({name: "Mr. A"});
+      chai.request('localhost:3000')
+      .post('/api/data')
+      .send(testQuote)
+      .end(function(err, res) {
+        expect(err).to.eql(null);
+        expect(typeof res.body).to.eql('object');
+        done();
+      });
+    });
+
+    it('PUT request', function(done) {
+      var testQuote = new Quote({name: "Mr. A"});
+      chai.request('localhost:3000')
+      .put('/api/data')
+      .send(testQuote)
+      .end(function(err, res) {
+        expect(err).to.eql(null);
+        expect(typeof res.body).to.eql('object');
+        done();
+      });
+    });
+
+    it('PATCH request', function(done) {
+      var testQuote = new Quote({name: "Mr. A"});
+      chai.request('localhost:3000')
+      .patch('/api/data')
+      .send(testQuote)
+      .end(function(err, res) {
+        expect(err).to.eql(null);
+        done();
+      }); 
+    });
+
+    it('DELETE request', function(done) {
+      var testQuote = new Quote({name: "Mr. A"});
+      chai.request('localhost:3000')
+      .del('/api/data')
+      .end(function(err, res) {
+        expect(err).to.eql(null);
+        done();
+      });  
+    });
 });
